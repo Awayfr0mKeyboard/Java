cef33b8c7298c8d3312abdfa8eae41e489269bbd
Awayfr0mkeyboard <seohi9301@gmail.com>
2024-05-30 15:50:00 +0900

    2024-05-30

---
+++ j17_generic/src/generic01_type/WatermelonBox.java
@@ -0,0 +1,18 @@
+package generic01_type;
+
+public class WatermelonBox {
+
+	private Watermelon watermelon;
+	
+	// getter setter 추가
+	public Watermelon getWatermelon() {
+		return watermelon;
+	}
+
+	public void setWatermelon(Watermelon watermelon) {
+		this.watermelon = watermelon;
+	}
+	
+	
+	
+}
