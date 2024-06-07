6b19bd872fb2a2a224d77c465975c96dba51675c
Awayfr0mkeyboard <seohi9301@gmail.com>
2024-06-07 16:10:47 +0900

    2024-06-07

--- j20_collection_framework_practice/src/guide_answer/practice02/Nation.java
+++ j20_collection_framework_practice/src/guide_answer/practice02/Nation.java
@@ -28,4 +28,20 @@ public class Nation {
 	public String getCapital() {
 		return capital;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// 매개변수로 전달된 Object obj 변수에 저장된 객체가
+		// 비교할 수 있는 Nation 객체면
+		// 국가 이름 정보가 일치하면 동등한 객체로 판별할 수 있도록 재정의
+		if (obj instanceof Nation) {
+			Nation n = (Nation)obj;
+			if (this.country.equals(n.getCountry())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
 }
